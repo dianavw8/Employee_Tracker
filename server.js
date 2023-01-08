6ef5f1313f455a45eb54769d30f04eedec2fabf2
Diana <dianavw8@gmail.com>
2023-01-08 17:30:18 -0500

    added gitignore, added inquirer prompt and getallDept, Roles, Ee functions

--- server.js
+++ server.js
@@ -1,2 +1,56 @@
-// Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'Pulgz03',
+    database: 'eetracker_db'
+  },
+  console.log(`Connected to the eetracker_db database.`)
+);
+
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to do today?',
+    choices: ['View all departments', 'View all roles', 'View all employees']
+  }
+]).then(answers => {
+  // The user's answer is stored in answers.action
+  if (answers.action === 'View all departments') {
+   getAllDept();  
+  }
+  if (answers.action === 'View all roles') {
+    getAllRoles();  
+   }
+   if (answers.action === 'View all employees') {
+    getAllEe();  
+   }
+});
+
+function  getAllDept(){
+// Query MySQL database and display all entries in the departments table
+db.query('SELECT * FROM departments', function (err, results) {
+    console.log(results);
+  });
+}
+
+function  getAllRoles(){
+// Query MySQL database and display all entries in the departments table
+db.query('SELECT * FROM roles', function (err, results) {
+    console.log(results);
+  });
+}
+
+function  getAllEe(){
+// Query MySQL database and display all entries in the departments table
+db.query('SELECT * FROM employees', function (err, results) {
+    console.log(results);
+  });
+}
